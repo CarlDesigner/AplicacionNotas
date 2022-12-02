c0f45dc1095dcbcc2c32cb3a6d567eaa249c036b
CarlDesigner <krlitosfajardo626@gmail.com>
2022-12-01 19:46:45 -0500

    Se establece la conexion a la BD

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const { listen } = require("./server");
+require("dotenv").config();
+
+
 const app = require("./server");
+require("./database")
 
 app.listen(app.get("port"), () => {
     console.log("Servidor en el puerto", app.get("port"));
