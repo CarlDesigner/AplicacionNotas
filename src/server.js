a132f931f7117cc5fc8f7391a1a09b2d626552ac
CarlDesigner <krlitosfajardo626@gmail.com>
2022-12-04 14:21:02 -0500

    Se crean los Handlebars

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
 const path = require("path");
+
 
 //Inicializacion
 const app = express();
@@ -7,7 +10,16 @@ const app = express();
 
 //Settings
 app.set("port", process.env.PORT || 4000);
-app.set("views", path.join(__dirname, "/wiews"));
+app.set("views", path.join(__dirname, "/views"));
+
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+
+app.set("view engine", ".hbs")
 
 
 //Middlewares
@@ -19,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("index")
 });
 
 //Static Files
