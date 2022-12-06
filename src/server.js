3de569cff73e5ba3a6f525dad740577992a09cf0
CarlDesigner <krlitosfajardo626@gmail.com>
2022-12-05 20:11:27 -0500

    Handlebars y el formulario para crear Notas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-
 const path = require("path");
+const morgan = require("morgan");
 
 
 //Inicializacion
@@ -23,6 +23,7 @@ app.set("view engine", ".hbs")
 
 
 //Middlewares
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 
 
