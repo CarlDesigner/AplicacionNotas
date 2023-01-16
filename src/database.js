9a6386300f579d75fb4faa8dce547205c3ea5a05
CarlDesigner <krlitosfajardo626@gmail.com>
2023-01-16 09:35:46 -0500

    Se agrega un bg blurry

--- src/database.js
+++ src/database.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
 const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+// const MONGODB_URI = `mongodb+srv://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
 mongoose.connect(MONGODB_URI, {
 
 })
     .then(db => console.log("Base de datos conectada"))
     .catch(err => console.log(err));
-
